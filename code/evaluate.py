7470df3040793a16cc603a59f96f17cd29a290a6
wanchichang <a0965266902@gmail.com>
2024-09-04 21:52:39 +0800

    fix: eval with custom object

--- code/evaluate.py
+++ code/evaluate.py
@@ -1,25 +1,34 @@
 import tensorflow as tf
 import numpy as np
 import os
+from playability import (
+    custom_loss_with_playability,
+)  # 确保自定义损失函数已经正确定义并导入
 
-model_folder = "my_model_0707"
-# 加载保存的模型
-model = tf.keras.models.load_model(model_folder)
+model_folder = "my_model_0817"
+
+# 加载保存的模型，并指定自定义损失函数
+model = tf.keras.models.load_model(
+    model_folder,
+    # custom_objects={"loss": custom_loss_with_playability},
+    compile=False,
+)
+model.compile(loss=custom_loss_with_playability, optimizer="adam", metrics="accuracy")
 
 # 加载测试数据
 X_test = np.load("/home/wanchichang/piano-reduction/LOP_database/X_test.npy")
 Y_test = np.load("/home/wanchichang/piano-reduction/LOP_database/Y_test.npy")
 X_test = np.transpose(X_test, (0, 2, 3, 1))
 Y_test = np.transpose(Y_test, (0, 2, 3, 1))
+
 # 评估模型
-loss, accuracy = model.evaluate(X_test, Y_test, batch_size=16)
+loss, accuracy = model.evaluate(X_test, Y_test, batch_size=16, verbose=1)
 
 # 打印结果
 print(f"Test loss: {loss}")
 print(f"Test accuracy: {accuracy}")
 
 # 指定保存路径
-# output_folder = "your_output_folder"  # 替换为实际的输出文件夹名称
 save_path = f"/home/wanchichang/piano-reduction/code/{model_folder}"
 
 # 确保目录存在
