f59a72814f8344a1fbd0a84c48d82b56c2242470
wanchichang <a0965266902@gmail.com>
2024-09-25 12:34:41 +0800

    style: remove comment

--- code/playability.py
+++ code/playability.py
@@ -49,66 +49,6 @@ def within_range(tensor, k):
     return out_of_range
 
 
-# # 示例使用
-# tensor = tf.constant([0, 1, 0, 1, 0, 0, 1], dtype=tf.float32)
-# max_pitch = 3
-# min_pitch = 2
-# k = 2
-
-# print("Any pitch out of range:", within_range(tensor, max_pitch, min_pitch, k).numpy())
-
-
-# # 定义 calculate_hand_span 函数
-# def calculate_hand_span(y_pred, k1=8, k2=10):
-#     batch_size, timesteps, num_pitches, _ = y_pred.shape
-#     max_hand_spans = []
-
-#     for i in range(batch_size):
-#         max_hand_span_for_batch = tf.constant(0.0, dtype=tf.float32)
-
-#         for t in range(timesteps):
-#             timestep_data = y_pred[i, t, :, 0]  # (num_pitches,)
-#             indices = tf.range(num_pitches, dtype=tf.float32)
-
-#             # 计算当前时间步的最大音高和最小音高
-#             max_pitch = tf.reduce_max(tf.where(timestep_data > 0, indices, -1))
-#             min_pitch = tf.reduce_min(tf.where(timestep_data > 0, indices, num_pitches))
-
-#             # 检查 handspan <= 6
-#             out_of_range_k1 = within_range(timestep_data, k1)
-#             handspan_for_timestep = tf.cond(
-#                 tf.logical_not(out_of_range_k1),
-#                 lambda: tf.constant(6.0, dtype=tf.float32),
-#                 lambda: tf.cond(
-#                     tf.logical_not(within_range(timestep_data, k2)),
-#                     lambda: tf.constant(10.0, dtype=tf.float32),
-#                     lambda: tf.constant(11.0, dtype=tf.float32),  # handspan > 10
-#                 ),
-#             )
-
-#             # 更新当前 batch 的最大 handspan
-#             max_hand_span_for_batch = tf.maximum(
-#                 max_hand_span_for_batch, handspan_for_timestep
-#             )
-
-#         # 根据最大手跨度为 batch 计算分数
-#         handspan_score = tf.cond(
-#             max_hand_span_for_batch <= 6,
-#             lambda: tf.constant(0.0, dtype=tf.float32),
-#             lambda: tf.cond(
-#                 max_hand_span_for_batch <= 10,
-#                 lambda: tf.constant(0.5, dtype=tf.float32),
-#                 lambda: tf.constant(1.0, dtype=tf.float32),
-#             ),
-#         )
-
-#         max_hand_spans.append(handspan_score)
-
-#     # 计算所有 batch 的平均 handspan score
-#     avg_hand_span_score = tf.reduce_mean(max_hand_spans)
-#     return avg_hand_span_score
-
-
 def calculate_hand_span(y_pred, k1=8, k2=10):
     batch_size, timesteps, num_pitches, _ = y_pred.shape
     max_hand_spans = []
@@ -149,41 +89,6 @@ def calculate_hand_span(y_pred, k1=8, k2=10):
 
     avg_hand_span_score = tf.reduce_mean(max_hand_spans)
     return avg_hand_span_score
-
-
-# def calculate_num_of_notes_score(y_pred):
-#     batch_size, timesteps, num_pitches, _ = y_pred.shape
-
-#     # 初始化一个张量来存储每个batch的最大音符数量
-#     max_num_of_notes_per_batch = tf.zeros((batch_size,), dtype=tf.float32)
-
-#     for t in range(timesteps):
-#         # 提取当前时间步的数据
-#         timestep_data = y_pred[:, t, :, :]  # 形状: (batch_size, num_pitches, 1)
-
-#         # 计算当前时间步的音符数量
-#         num_of_notes = tf.reduce_sum(timestep_data, axis=[1, 2])  # 形状: (batch_size,)
-
-#         # 更新每个batch的最大音符数量
-#         max_num_of_notes_per_batch = tf.maximum(
-#             max_num_of_notes_per_batch, num_of_notes
-#         )
-
-#     # 计算所有batch的最大num_of_notes的平均值
-#     avg_max_num_of_notes = tf.reduce_mean(max_num_of_notes_per_batch)
-
-#     # 根据平均值判断num_of_notes_score
-#     num_of_notes_score = tf.cond(
-#         avg_max_num_of_notes > 10,
-#         lambda: tf.constant(0.0, dtype=tf.float32),
-#         lambda: tf.cond(
-#             avg_max_num_of_notes > 6,
-#             lambda: tf.constant(0.5, dtype=tf.float32),
-#             lambda: tf.constant(1.0, dtype=tf.float32),
-#         ),
-#     )
-
-#     return num_of_notes_score
 
 
 def calculate_num_of_notes_score(y_pred):
